1a2dc42301d509b46ea23cc6d2d80f6a5b9508eb
mgalarowicz <rezi610@gmail.com>
2022-11-17 20:41:29 +0100

    Initial commit

---
+++ src/Bootstrapper/Confab.Bootstrapper/Startup.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Confab.Bootstrapper
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGet("/", context => context.Response.WriteAsync("Hello World!"));
+            });
+        }
+    }
+}
