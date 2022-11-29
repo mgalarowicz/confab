cd33efa513d75fff89d72de6d3a1e1724f6fdceb
mgalarowicz <marek.galarowicz@gmail.com>
2022-11-29 20:03:43 +0100

    .Net7 + base layers

--- src/Bootstrapper/Confab.Bootstrapper/Program.cs
+++ src/Bootstrapper/Confab.Bootstrapper/Program.cs
@@ -1,16 +1,14 @@
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Confab.Modules.Conferences.Api;
+using Confab.Shared.Infrastructure;
 
-namespace Confab.Bootstrapper
-{
-    public class Program
-    {
-        public static Task Main(string[] args)
-            => CreateHostBuilder(args).Build().RunAsync();
-        
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
-}
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddInfrastructure();
+services.AddConferences();
+
+var app = builder.Build();
+
+app.UseInfrastructure();
+
+app.Run();
